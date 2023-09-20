07d5eaf147fe14ed9f617aeaa8d67caf833100cd
DirgaNugraha <akunnyadirga@gmail.com>
2023-09-20 14:17:19 +0800

    Tugas Praktikum ke2

--- H071231039/Praktikum-2/TP2_2_H071231039.py
+++ H071231039/Praktikum-2/TP2_2_H071231039.py
@@ -1,25 +1,23 @@
-x= (input('Masukkan Input Pertama: '))
-y= (input('Masukkan Input Kedua: '))
-z= (input('Masukkan Input Ketiga: '))
+x = (input('Masukkan Input Pertama: '))
+y = (input('Masukkan Input Kedua: '))
+z = (input('Masukkan Input Ketiga: '))
 
-match x,y,z:
-    case 'vertebrado','ave','carnivoro':
+match x, y, z:
+    case 'vertebrado', 'ave', 'carnivoro':
         print('agula')
-    case 'vertebrado','ave','onivoro':
+    case 'vertebrado', 'ave', 'onivoro':
         print('pomba')
-    case 'vertebrado','mamifero','onivoro':
+    case 'vertebrado', 'mamifero', 'onivoro':
         print('homem')
-    case 'vertebrado','mamifero','herbivoro':
+    case 'vertebrado', 'mamifero', 'herbivoro':
         print('vaca')
-    case 'ivertebrado','inseto','hematofago':
+    case 'ivertebrado', 'inseto', 'hematofago':
         print('pulga')
-    case 'invertebrado','inseto','herbivoro':
+    case 'invertebrado', 'inseto', 'herbivoro':
         print('lagarta')
-    case 'invertebrado','anelideo','hematofago':
+    case 'invertebrado', 'anelideo', 'hematofago':
         print('sanguessuga')
-    case 'invertebrado','anelideo','onivoro':
+    case 'invertebrado', 'anelideo', 'onivoro':
         print('minhoca')
     case _:
         print('Invalid input')
-
-
