704390ac421aad5e79ceeb6b48909306509645cc
DirgaNugraha <akunnyadirga@gmail.com>
2023-10-06 23:24:34 +0800

    Tugas Praktikum ke-4

---
+++ H071231039/Praktikum-4/TP4_5_H071231039.py
@@ -0,0 +1,39 @@
+def fibonacci(angka,a=0,b=1):
+   if angka > 0:
+        print(a, end= " ")
+        c = a+b
+        a = b
+        b = c
+        fibonacci(angka-1,a,b)
+
+
+print('='*40)
+print('Selamat datang di program fibonacci')
+print('='*40)
+
+
+# menu = int(input())
+while True:
+    print('\nSilahkan pilih menu\n1.Default Number\n2.Custom Number\n3.Exit')
+    menu = int(input('\n= '))
+    if menu == 1:
+        menu1 = int(input('Masukkan angka= '))
+        fibonacci(menu1)
+
+    elif menu == 2:
+        iterasi= int(input('Masukkan iterasi= '))
+        angkasatu= int(input('Masukkan angka pertama= '))
+        angkadua= int(input('Masukkan angka kedua= '))
+        fibonacci(iterasi,angkasatu,angkadua)
+
+    elif menu == 3:
+        print('Terima Kasih')
+        exit()
+
+    else:
+        print('Menu tidak tersedia')
+
+    
+        
+
+
