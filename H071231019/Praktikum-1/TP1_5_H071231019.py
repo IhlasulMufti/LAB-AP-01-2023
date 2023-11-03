f7ab3e4f129e0709b430cd8b9a2d89b4d2a3c288
Musliati <143779973+Musliati@users.noreply.github.com>
2023-11-03 18:37:49 +0800

    Delete H071231019 directory

--- H071231019/Praktikum-1/TP1_5_H071231019.py
+++
@@ -1,9 +0,0 @@
-print("Konversi detik ke jam")
-d = int(input("Masukkan jumlah detik: "))
-
-m = d // 60 #hasil pembagian bulat d dan 60 dimasukkan ke variabel m
-sisa_detik = d % 60
-j = m // 60 #hasil pembagian bulat m dan 60 dimasukkan ke variabel j
-sisa_menit = m % 60
-
-print("{:02}:{:02}:{:02}".format(j,sisa_menit,sisa_detik))
